5836ade0228cce32448442ee08cc6ea18e46601d
Kutman <xsmaiks@gmail.com>
2025-04-18 23:36:39 +0600

    test

--- boiler_business/settings.py
+++ boiler_business/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 STATIC_ROOT = 'static_files/'
 
 # Default primary key field type
