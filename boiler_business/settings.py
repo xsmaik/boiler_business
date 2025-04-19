1adcd0ac890d5451aeace40bde66966a721438d2
Kutman <xsmaiks@gmail.com>
2025-04-19 22:53:01 +0600

    test

--- boiler_business/settings.py
+++ boiler_business/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 STATIC_ROOT = 'static_files/'
 
 # Default primary key field type
@@ -134,4 +134,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
